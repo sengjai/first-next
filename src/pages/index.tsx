d092e22de3fa79dbb7798b7467cada2c37f2d987
sengjai <soonseng.ee@thetechyhub.com>
2024-02-10 10:25:05 +0800

    Looking at server side props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
-import Link from 'next/link'
+//import sleep from 'await-sleep';
 
-
-export default function Home() {
+export default function Home(props: any) {
     return (
-    <>
-        <h1>Welcome to Home</h1>
-        <p>Lorem ipsum, welcome to about page</p>
-        <Link href="/about">Go to about</Link>
-    </>
-    
-    
+      <div>
+        <h2>Welcome to our homepage.</h2>
+        <p>This is the best home page in the world. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum aspernatur illum architecto cumque recusandae fuga sequi necessitatibus deleniti repellat harum nobis, dolor veniam vero deserunt. Voluptatibus, ducimus deserunt. Recusandae, dolore.</p>
+        <p>The weather: {props.forecast}</p>
+      </div>
     )
-}
+  }
+  
+  //this is for data that needs to be not cache
+  export async function getServerSideProps() {
+    const response = await fetch("https://api.weather.gov/gridpoints/MFL/109,49/forecast")
+    const data = await response.json()
+
+    //await sleep(3000)
+  
+    return {
+      props: {
+        forecast: data.properties.periods[0].detailedForecast
+      }
+    }
+  }
